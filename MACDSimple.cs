6a3790b697362d924460a4f39f2cada35f9ba81f
Eric Wang <wangzy1986@gmail.com>
2014-11-19 20:25:12 +0800

    1st version of macd divergence

--- MACDSimple.cs
+++ MACDSimple.cs
@@ -30,12 +30,17 @@ namespace NinjaTrader.Strategy
         // User defined variables (add any user defined variables below)
 		private double StopLossPrice;
 		private double TakeProfitPrice;
+		private double MACDPreviousHigh;
+		private double MACDPreviousLow;
+		private double EMAPreviousHigh;
+		private double EMAPreviousLow;
 		private bool MACDCrossAbove = false;
 		private bool MACDCrossBelow = false;
-		private bool PriceNewHigh = false;
-		private bool PriceNewLow = false;
-		private bool MACDNewHigh = false;
-		private bool MACDNewLow = false;
+		private int LastCrossBar = 0;
+//		private bool EMANewHigh = false;
+//		private bool EMANewLow = false;
+//		private bool MACDNewHigh = false;
+//		private bool MACDNewLow = false;
         #endregion
 
         /// <summary>
@@ -74,37 +79,127 @@ namespace NinjaTrader.Strategy
 			{
 				MACDCrossBelow = false;
 			}
+//			if ((MACD(MACDFast, MACDSlow, MACDSingle).Avg[4] < MACD(MACDFast, MACDSlow, MACDSingle).Avg[2])
+//				&& (MACD(MACDFast, MACDSlow, MACDSingle).Avg[2] > MACD(MACDFast, MACDSlow, MACDSingle).Avg[0]))
+//			{
+//				if (MACD(MACDFast, MACDSlow, MACDSingle).Avg[2] > MACDPreviousHigh)
+//				{
+//					MACDNewHigh = true;
+//				}
+//				else
+//				{
+//					MACDNewHigh = false;
+//				}
+//				MACDPreviousHigh = MACD(MACDFast, MACDSlow, MACDSingle).Avg[2];
+//			}
+//			if ((MACD(MACDFast, MACDSlow, MACDSingle).Avg[4] > MACD(MACDFast, MACDSlow, MACDSingle).Avg[2])
+//				&& (MACD(MACDFast, MACDSlow, MACDSingle).Avg[2] < MACD(MACDFast, MACDSlow, MACDSingle).Avg[0]))
+//			{
+//				if (MACD(MACDFast, MACDSlow, MACDSingle).Avg[2] < MACDPreviousLow)
+//				{
+//					MACDNewLow = true;
+//				}
+//				else
+//				{
+//					MACDNewLow = false;
+//				}
+//				MACDPreviousLow = MACD(MACDFast, MACDSlow, MACDSingle).Avg[2];
+//			}
+//			if ((EMA(Close,12)[4] < EMA(Close,12)[2])
+//				&& (EMA(Close,12)[2] > EMA(Close,12)[0]))
+//			{
+//				if (EMA(Close,12)[2] > EMAPreviousHigh)
+//				{
+//					EMANewHigh = true;
+//				}
+//				else
+//				{
+//					EMANewHigh = false;
+//				}
+//				EMAPreviousHigh = EMA(Close,12)[2];
+//			}
+//			if ((EMA(Close,12)[4] > EMA(Close,12)[2])
+//				&& (EMA(Close,12)[2] < EMA(Close,12)[0]))
+//			{
+//				if (EMA(Close,12)[2] < EMAPreviousLow)
+//				{
+//					EMANewLow = true;
+//				}
+//				else
+//				{
+//					EMANewLow = false;
+//				}
+//				EMAPreviousLow = EMA(Close,12)[2];
+//			}
 			
             // Enter Market
-            if (MACDCrossAbove)
-            {
-                EnterLong(DefaultQuantity, "");
-				StopLossPrice = DonchianChannel(DonchianPeriod).Lower[1];
-				if (StopLossPrice > Close[0])
-				{StopLossPrice = 0;}					
-				TakeProfitPrice = (Close[0]-DonchianChannel(DonchianPeriod).Lower[1])*2+Close[0];
-            }
-            if (MACDCrossBelow)
-            {
-                EnterShort(DefaultQuantity, "");
-				StopLossPrice = DonchianChannel(DonchianPeriod).Upper[1];
-				if (StopLossPrice < Close[0])
-				{StopLossPrice = 0;}
-				TakeProfitPrice = Close[0]-(DonchianChannel(DonchianPeriod).Upper[1]-Close[0])*2;
-            }
-			if (MACDNewHigh && (!PriceNewHigh))
+//            if (MACDCrossAbove)
+//            {
+//                EnterLong(DefaultQuantity, "");
+//				StopLossPrice = DonchianChannel(DonchianPeriod).Lower[1];
+//				if (StopLossPrice > Close[0])
+//				{StopLossPrice = 0;}					
+//				TakeProfitPrice = (Close[0]-DonchianChannel(DonchianPeriod).Lower[1])*2+Close[0];
+//            }
+//            if (MACDCrossBelow)
+//            {
+//                EnterShort(DefaultQuantity, "");
+//				StopLossPrice = DonchianChannel(DonchianPeriod).Upper[1];
+//				if (StopLossPrice < Close[0])
+//				{StopLossPrice = 0;}
+//				TakeProfitPrice = Close[0]-(DonchianChannel(DonchianPeriod).Upper[1]-Close[0])*2;
+//            }
+////			if (MACDNewHigh && (!EMANewHigh)
+////				&& Position.MarketPosition == MarketPosition.Flat)
+////			{
+////				EnterLong(DefaultQuantity, "");
+////				StopLossPrice = DonchianChannel(DonchianPeriod).Lower[1];
+////				if (StopLossPrice > Close[0])
+////				{StopLossPrice = 0;}		
+////			}
+////			if (MACDNewLow && (!EMANewLow)
+////				&& Position.MarketPosition == MarketPosition.Flat)
+////			{
+////				EnterShort(DefaultQuantity, "");
+////				StopLossPrice = DonchianChannel(DonchianPeriod).Upper[1];
+////				if (StopLossPrice < Close[0])
+////				{StopLossPrice = 0;}		
+////			}
+			// MACD Divergences
+			if (CrossAbove(MACD(MACDFast, MACDSlow, MACDSingle).Diff, 0, 1))
 			{
-				EnterLong(DefaultQuantity, "");
-				StopLossPrice = DonchianChannel(DonchianPeriod).Lower[1];
-				if (StopLossPrice > Close[0])
-				{StopLossPrice = 0;}		
+				if (LastCrossBar > 0)
+				{
+					int BarDiff = CurrentBars[0] - LastCrossBar;
+					Log("CrossAbove, " + Time[0] 
+					+ " Close = " + Close[0].ToString()
+					+ " MACD = " + MACD(MACDFast, MACDSlow, MACDSingle).Diff[0].ToString()
+					+ ", BarNumber = " + CurrentBars[0].ToString()
+					+ ", BarDiff = " + BarDiff.ToString(), LogLevel.Information);
+					if (Close[0] < Close[BarDiff]
+						&& MACD(MACDFast, MACDSlow, MACDSingle).Diff[0] > MACD(MACDFast, MACDSlow, MACDSingle).Diff[BarDiff] )
+					{
+						EnterShort(DefaultQuantity, "");
+					}
+				}
+				LastCrossBar = CurrentBars[0];
 			}
-			if (MACDNewLow && (!PriceNewLow))
+			if (CrossBelow(MACD(MACDFast, MACDSlow, MACDSingle).Diff, 0, 1))
 			{
-				EnterShort(DefaultQuantity, "");
-				StopLossPrice = DonchianChannel(DonchianPeriod).Upper[1];
-				if (StopLossPrice < Close[0])
-				{StopLossPrice = 0;}		
+				if (LastCrossBar > 0)
+				{
+					int BarDiff = CurrentBars[0]-LastCrossBar;
+					Log("CrossBelow, " +Time[0] 
+					+ " Close = " + Close[0].ToString()
+					+ " MACD = " + MACD(MACDFast, MACDSlow, MACDSingle).Diff[0].ToString()
+					+ ", BarNumber = "+CurrentBars[0].ToString()+", BarDiff = "+BarDiff.ToString(), LogLevel.Information);
+					if (Close[0] > Close[BarDiff]
+						&& MACD(MACDFast, MACDSlow, MACDSingle).Diff[0] < MACD(MACDFast, MACDSlow, MACDSingle).Diff[BarDiff] )
+					{
+						EnterLong(DefaultQuantity, "");
+					}
+				}
+				LastCrossBar = CurrentBars[0];
 			}
 			
 
